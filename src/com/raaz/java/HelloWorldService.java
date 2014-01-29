3b9f4cc862737e09e1b32b9397adaefc22751576
RaazTripathi <raaz19.tripathi@gmail.com>
2014-01-29 19:48:59 +0530

    Update HelloWorldService.java

--- src/com/raaz/java/HelloWorldService.java
+++ src/com/raaz/java/HelloWorldService.java
@@ -14,9 +14,9 @@ public class HelloWorldService
 	public Response getMsg(@PathParam("param") String msg) {
  
  
- 
+ /*
 		String output = "Jersey say : " + msg;
- 
+ */
 		return Response.status(200).entity(output).build();
  
 	}
